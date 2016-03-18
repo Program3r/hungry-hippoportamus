b6d377de22c9a5eb0c98e50e55925166062480b0
Program3r <josh@findjosh.com>
2016-03-18 07:28:00 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --restart=always --name="service-discovery" --privileged -t -i -d --net=host service-discovery forever -o out.log -e err.log index.js
+docker run --restart=always --name="service-discovery" --privileged -t -i -d --net=host program3r/hungry-hippoportamus forever -o out.log -e err.log index.js
